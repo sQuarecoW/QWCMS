eefe1e5aa8df4b02c891a4547897ce26993452c4
Jan-Willem Oskam <jw@squarecow.nl>
2020-02-10 09:20:42 +0100

    Added the ability to show just the date in the date picker form field

--- Sources/QWCMS/CMS/Tags/DatePickerTag.swift
+++ Sources/QWCMS/CMS/Tags/DatePickerTag.swift
@@ -32,44 +32,56 @@ public final class DatePickerTag: TagRenderer {
             minuteValue = date.component(.minute)
         }
         
+        // do we show a timepicker?
+        var showTime = true
+        if tag.parameters.count > 1, let s = tag.parameters[1].bool {
+            showTime = s
+        }
+        
         var html =
             """
             <div class="col-auto">
-                <div class="input-group datepicker">
-                    <input type="text" class="form-control date" placeholder="Datum" value="\(dateValue)">
+                <div class="input-group datepicker datepicker-popover">
+                    <input type="text" class="form-control date" name="date" placeholder="Datum" value="\(dateValue)">
                     <div class="input-group-append">
                         <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
                     </div>
                 </div>
             </div>
-            <div class="col-auto">
-                <div class="input-group">
-                  <select class="custom-select hour">
             """
         
-        for i in 0...23 {
-            html += "<option value=\"\(i < 10 ? "0" : "")\(i)\"\(i == hourValue ? " selected=\"selected\"" : "")>\(i < 10 ? "0" : "")\(i)</option>"
-        }
-        
-        html +=
-            """
-                  </select>
-                  <select class="custom-select minute">
-            """
-        
-        for i in 0...59 {
-            html += "<option value=\"\(i < 10 ? "0" : "")\(i)\"\(i == minuteValue ? " selected=\"selected\"" : "")>\(i < 10 ? "0" : "")\(i)</option>"
-        }
-        
-        html +=
-            """
-                  </select>
-                  <div class="input-group-append">
-                    <span class="input-group-text"><i class="far fa-clock"></i></span>
-                  </div>
+        if showTime {
+                html +=
+                """
+                <div class="col-auto">
+                    <div class="input-group">
+                      <select class="custom-select hour">
+                """
+            
+            for i in 0...23 {
+                html += "<option value=\"\(i < 10 ? "0" : "")\(i)\"\(i == hourValue ? " selected=\"selected\"" : "")>\(i < 10 ? "0" : "")\(i)</option>"
+            }
+            
+            html +=
+                """
+                      </select>
+                      <select class="custom-select minute">
+                """
+            
+            for i in 0...59 {
+                html += "<option value=\"\(i < 10 ? "0" : "")\(i)\"\(i == minuteValue ? " selected=\"selected\"" : "")>\(i < 10 ? "0" : "")\(i)</option>"
+            }
+            
+            html +=
+                """
+                      </select>
+                      <div class="input-group-append">
+                        <span class="input-group-text"><i class="far fa-clock"></i></span>
+                      </div>
+                    </div>
                 </div>
-            </div>
-            """
+                """
+        }
         
         return Future.map(on: tag) { return .string(html) }
     }
