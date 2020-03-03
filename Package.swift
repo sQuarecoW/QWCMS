34cb8e4641993458e6c507b65bfe3c6d7275169f
Jan-Willem Oskam <jw@squarecow.nl>
2020-03-03 14:19:05 +0100

    Whoops

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "QWCMS",
-            dependencies: ["Vapor", "Leaf", "FluentPostgreSQL", "Authentication"]),
+            dependencies: ["Vapor", "Leaf", "FluentPostgreSQL", "Authentication", "Redis"]),
         .testTarget(
             name: "QWCMSTests",
             dependencies: ["QWCMS"]),
