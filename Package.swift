1a078dbc0c1d6a9bfbaff377e19ea2324f93f677
Jan-Willem Oskam <jw@squarecow.nl>
2020-03-30 12:44:51 +0200

    Updated package.swift for Vapor 4

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "QWCMS",
+    platforms: [
+        .macOS(.v10_15),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -13,25 +16,31 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc"),
         
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0-rc"),
         
-        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
-        
-        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0"),
+        // Fluent
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-rc"),
+        // Postgresql
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0-rc"),
         
         // Redis
-        .package(url: "https://github.com/vapor/redis.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/redis.git", from: "4.0.0-beta.5"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "QWCMS",
-            dependencies: ["Vapor", "Leaf", "FluentPostgreSQL", "Authentication", "Redis"]),
-        .testTarget(
-            name: "QWCMSTests",
-            dependencies: ["QWCMS"]),
+            dependencies: [
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "Redis", package: "redis")
+        ]),
+        .testTarget(name: "AppTests", dependencies: [
+            .target(name: "App"),
+        ])
     ]
 )
